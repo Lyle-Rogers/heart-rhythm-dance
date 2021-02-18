e9883b3de90940d51c219e14a13aefd3d0990ec1
Lyle-Rogers <lylerogers8@gmail.com>
2021-02-18 16:02:23 -0700

    Added not signed in handling, small fixes, and maid the calendar more dinamic.

---
+++ start/hooks.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const { hooks } = require('@adonisjs/ignitor')
+const moment = require('moment')
+
+hooks.after.providersBooted(() => {
+  const Exception = use('Exception')
+
+  Exception.handle('InvalidSessionException', ({ response }) => {
+    return response.redirect('/sign_in')
+  })
+})
+
+hooks.after.providersBooted(() => {
+  const View = use('View')
+
+  View.global('thisMonth', () => {
+    return moment().format('MMMM')
+  })
+})
--- start/routes.js
+++ start/routes.js
@@ -16,7 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('auth/sign_in')
+Route.get('/', 'BlogController.loadBlog').middleware(['auth'])
 
 Route.on('/create_user').render('auth.create_user')
 Route.post('/create_user', 'SignInController.createUser').validator('CreateUser')
@@ -29,24 +29,24 @@ Route.get('/sign_out', async ({ auth, response }) => {
   return response.redirect('back');
 })
 
-Route.on('/register').render('pages/register')
+Route.on('/register').render('pages/register').middleware(['auth'])
 Route.post('/register', 'RegisterController.register')
 
-Route.get('/your_registers', 'RegisterController.loadRegisters')
+Route.get('/your_registers', 'RegisterController.loadRegisters').middleware(['auth'])
 Route.get('/your_registers/delete_click/:id', 'RegisterController.deleteBtnClick')
 Route.get('/your_registers/delete/:id', 'RegisterController.deleteRegistraition')
 Route.get('/your_register_edit/:id', 'RegisterController.editBtnClick')
 Route.post('/update_registration/:id', 'RegisterController.updateRegistration')
 
-Route.get('/admin', 'AdminController.loadAdminPage')
+Route.get('/admin', 'AdminController.loadAdminPage').middleware(['auth'])
 
-Route.get('/blog', 'BlogController.loadBlog')
+Route.get('/blog', 'BlogController.loadBlog').middleware(['auth'])
 Route.post('/blog', 'BlogController.sendBlog').validator('Blog')
 Route.get('/blog/blog_delete/:id', 'BlogController.deleteBlog')
 Route.get('/blog/blog_edit/:id', 'BlogController.editBlog')
 Route.post('/blog/blog_update/:id', 'BlogController.updateBlog').validator('Blog')
 
-Route.get('blog_comments/:id', 'BlogController.loadComments')
+Route.get('blog_comments/:id', 'BlogController.loadComments').middleware(['auth'])
 Route.post('blog_comments/:id', 'BlogController.sendBlogComment').validator('BlogComments')
 Route.get('blog_comments/delete/:id', 'BlogController.deleteComment')
 
@@ -59,50 +59,50 @@ Route.get('/pay/success', 'PaymentController.paySuccess')
 
 Route.on('/calendar').render('calendar/months_list')
 
-Route.get('/january', 'CalendarController.loadJanuary')
+Route.get('/January', 'CalendarController.loadJanuary')
 Route.get('/january/:day', 'CalendarController.januaryDescriptionClick')
 Route.post('/january/change_day_description/:day', 'CalendarController.ChangeJanuaryDayDescription')
 
-Route.get('/february', 'CalendarController.loadFebruary')
+Route.get('/February', 'CalendarController.loadFebruary')
 Route.get('/february/:day', 'CalendarController.februaryDescriptionClick')
 Route.post('/february/change_day_description/:day', 'CalendarController.ChangeFebruaryDayDescription')
 
-Route.get('/march', 'CalendarController.loadMarch')
+Route.get('/March', 'CalendarController.loadMarch')
 Route.get('/march/:day', 'CalendarController.marchDescriptionClick')
 Route.post('/march/change_day_description/:day', 'CalendarController.ChangeMarchDayDescription')
 
-Route.get('/april', 'CalendarController.loadApril')
+Route.get('/April', 'CalendarController.loadApril')
 Route.get('/april/:day', 'CalendarController.aprilDescriptionClick')
 Route.post('/april/change_day_description/:day', 'CalendarController.ChangeAprilDayDescription')
 
-Route.get('/may', 'CalendarController.loadMay')
+Route.get('/May', 'CalendarController.loadMay')
 Route.get('/may/:day', 'CalendarController.mayDescriptionClick')
 Route.post('/may/change_day_description/:day', 'CalendarController.ChangeMayDayDescription')
 
-Route.get('/june', 'CalendarController.loadJune')
+Route.get('/June', 'CalendarController.loadJune')
 Route.get('/june/:day', 'CalendarController.juneDescriptionClick')
 Route.post('/june/change_day_description/:day', 'CalendarController.ChangeJuneDayDescription')
 
-Route.get('/july', 'CalendarController.loadJuly')
+Route.get('/July', 'CalendarController.loadJuly')
 Route.get('/july/:day', 'CalendarController.julyDescriptionClick')
 Route.post('/july/change_day_description/:day', 'CalendarController.ChangeJulyDayDescription')
 
-Route.get('/august', 'CalendarController.loadAugust')
+Route.get('/August', 'CalendarController.loadAugust')
 Route.get('/august/:day', 'CalendarController.augustDescriptionClick')
 Route.post('/august/change_day_description/:day', 'CalendarController.ChangeAugustDayDescription')
 
-Route.get('/september', 'CalendarController.loadSeptember')
+Route.get('/September', 'CalendarController.loadSeptember')
 Route.get('/september/:day', 'CalendarController.septemberDescriptionClick')
 Route.post('/september/change_day_description/:day', 'CalendarController.ChangeSeptemberDayDescription')
 
-Route.get('/october', 'CalendarController.loadOctober')
+Route.get('/October', 'CalendarController.loadOctober')
 Route.get('/october/:day', 'CalendarController.octoberDescriptionClick')
 Route.post('/october/change_day_description/:day', 'CalendarController.ChangeOctoberDayDescription')
 
-Route.get('/november', 'CalendarController.loadNovember')
+Route.get('/November', 'CalendarController.loadNovember')
 Route.get('/november/:day', 'CalendarController.novemberDescriptionClick')
 Route.post('/november/change_day_description/:day', 'CalendarController.ChangeNovemberDayDescription')
 
-Route.get('/december', 'CalendarController.loadDecember')
+Route.get('/December', 'CalendarController.loadDecember')
 Route.get('/december/:day', 'CalendarController.decemberDescriptionClick')
 Route.post('/december/change_day_description/:day', 'CalendarController.ChangeDecemberDayDescription')
